1098ff07373b7deab2e28c709d8b774c28ab4f0f
Andreas Wäscher <awaescher@gmx.de>
2026-03-12 22:46:31 +0100

    llmaid 1.0.1-7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/llmaid.rb
+++ Formula/llmaid.rb
@@ -1,9 +1,9 @@
 class Llmaid < Formula
   desc "Throw files against LLMs"
   homepage "https://github.com/awaescher/llmaid"
-  url "https://github.com/awaescher/llmaid/releases/download/1.0.1-6/llmaid-macos-arm64.tar.gz"
-  sha256 "22da91f5f21c51a55faf0d7ea2219ee4582d350e0ea5e7a3c5f0188237c71b24"
-  version "1.0.1-6"
+  url "https://github.com/awaescher/llmaid/releases/download/1.0.1-7/llmaid-macos-arm64.tar.gz"
+  sha256 "aa76d5da72a5c8672a7a184aa7e4bd5069faf0353e169cf0e1372812a838beec"
+  version "1.0.1-7"
 
   def install
     bin.install "llmaid"
